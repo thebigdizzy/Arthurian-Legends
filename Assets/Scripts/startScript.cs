2141eea7a1772605e0b5bcc2c150f5869b616547
thebigdizzy <dltrayler@mymail.tstc.edu>
2016-06-07 10:30:08 -0500

    Added more scenes for playability

---
+++ Assets/Scripts/startScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class startScript : MonoBehaviour {
+
+    public void startGame(string levelToLoad)
+    {
+        if (levelToLoad == "quit")
+        {
+            Application.Quit();
+        }
+        else {
+            Application.LoadLevel(levelToLoad);
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
